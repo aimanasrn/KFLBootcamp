4a46a92335802b45336fbefc989ed5c17698cae8
Muhammad Aiman bin Kanasronsham <96905225+aimanasrn@users.noreply.github.com>
2025-03-28 14:59:35 +0800

    nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+// import React from 'react';
+import { useState } from 'react';
 import { navLinks } from '../constants'
 import { Menu, X } from 'lucide-react';
 
